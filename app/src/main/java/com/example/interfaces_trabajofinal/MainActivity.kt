7f65310d8cece389ac9888df0e96b85c40e4ba47
AlumnoM <eduardoflores.9595@gmail.com>
2023-02-20 13:27:57 +0100

    first commit

--- app/src/main/java/com/example/interfaces_trabajofinal/MainActivity.kt
+++ app/src/main/java/com/example/interfaces_trabajofinal/MainActivity.kt
@@ -1,13 +1,29 @@
 package com.example.interfaces_trabajofinal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.interfaces_trabajofinal.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        /*binding.botonRegistrar.setOnClickListener {
+            val intentRegistrar = Intent(this, Login::class.java)
+            //intentCrearPers.putExtra("enviarImagenClase", enviarImagenClase)
+            startActivity(intentRegistrar)
+        }
+
+        binding.botonLogin.setOnClickListener {
+            val intentLogin = Intent(this, PaginaPrincipal::class.java)
+            //intentElegirPers.putExtra("enviarImagenClase", enviarImagenClase)
+            startActivity(intentLogin)
+        }*/
 
     }
 }
---
+++ app/src/main/java/com/example/interfaces_trabajofinal/PaginaReproductor.kt
@@ -0,0 +1,11 @@
+package com.example.interfaces_trabajofinal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PaginaReproductor : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pagina_reproductor)
+    }
+}
