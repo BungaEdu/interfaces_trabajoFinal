709e9d3ca9d3627d7391184ef8737e9c5a7ecc70
AlumnoM <eduardoflores.9595@gmail.com>
2023-02-22 14:07:32 +0100

    pantalla principal terminado, falta fking logo

--- app/src/main/java/com/example/interfaces_trabajofinal/Login.kt
+++ app/src/main/java/com/example/interfaces_trabajofinal/Login.kt
@@ -1,11 +1,26 @@
 package com.example.interfaces_trabajofinal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.interfaces_trabajofinal.databinding.ActivityMainBinding
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btPerfil1.setOnClickListener {
+            val intentMain = Intent(this, MainActivity::class.java)
+            startActivity(intentMain)
+        }
+
+        binding.btPerfil1.setOnClickListener {
+            val intentMain = Intent(this, MainActivity::class.java)
+            startActivity(intentMain)
+        }
     }
 }
